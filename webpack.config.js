70efbac0e77405bec6c68abe37518bec065c4ddc
max ulidtko <ulidtko@gmail.com>
2018-04-14 21:48:50 +0300

    generate index.html, add sounds

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
 
+let HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: [
+    // './src/index.html',
+    './src/index.js',
+  ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+        test: /sounds\/.*\.flac$/,
+        use: [ 'file-loader' ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      hash: true,
+      filename: './index.html',
+      template: './src/index.html',
+      title: 'A timer',
+    })
+  ]
 };
