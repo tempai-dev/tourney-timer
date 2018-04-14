b9092aff38f9648d38066c4f17bf66af4bb18f10
max ulidtko <ulidtko@gmail.com>
2018-04-15 01:31:41 +0300

    clock is ticking now

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import vue from 'vue';
+import moment from 'moment';
+require('moment-duration-format')(moment);
+
+const HANCHAN_DURATION = moment.duration(90, 'minutes')
+
+var gHanchanEnd = null; // TODO persist this
+
+function RefreshClock() {
+  let clock = document.querySelector('.clock')
+  let text = (!! gHanchanEnd)
+                ? moment.duration(gHanchanEnd.diff(moment())).format('hh:mm:ss')
+                : HANCHAN_DURATION.format('hh:mm:ss')
+  clock.innerText = text
+}
 
 function StartHanchanCountdown() {
   let tick = document.getElementById('tick')
@@ -11,16 +24,20 @@ function StartHanchanCountdown() {
   }
 
   let playOnce = function(audio) {
-    return function() {
-      audio.currentTime = 0
-      audio.play()
-      flashClock()
-    }
+    audio.currentTime = 0
+    audio.play()
+    flashClock()
   }
-  setTimeout(playOnce(tick), 1000)
-  setTimeout(playOnce(tick), 2000)
-  setTimeout(playOnce(tick), 3000)
-  setTimeout(playOnce(gong), 4000)
+
+  RefreshClock()
+  let startTimer = function() {
+    gHanchanEnd = moment().add(HANCHAN_DURATION)
+    setInterval(RefreshClock, 1000)
+  }
+  setTimeout(() => playOnce(tick), 1000)
+  setTimeout(() => playOnce(tick), 2000)
+  setTimeout(() => playOnce(tick), 3000)
+  setTimeout(() => { playOnce(gong); startTimer(); }, 4000)
   /* Yoroshiku Onegaishimasu! */
 }
 
